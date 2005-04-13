df1757e17c2da41bef23bd850cb699567f787ee0
whennings <whennings@e562ef82-0334-0410-8083-dbe22b1e9a75>
2005-04-13 10:06:01 +0000

    WH: additionally output clipping offset parameter to the latex2png call and support this parameter in the scripts.
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@526 e562ef82-0334-0410-8083-dbe22b1e9a75

--- scripts/latex2pn.bat
+++ scripts/latex2pn.bat
@@ -2,7 +2,7 @@
 rem echo latex2pn.bat (%1 %2 %3 %4 %5 %6 %7 %8 %9)
 rem This version uses latex and dvips
 rem              with convert (Part of ImageMagick)
-
+rem
 rem USAGE: latex2png -d density [-o offset] [-H home_dir] filename
 rem        where filename is the name (without extension) of the file to be converted,
 rem        either a LaTeX file or a eps file.
@@ -11,8 +11,6 @@ rem OPTIONS: -d density  (required! where density is in pixels per inch)
 rem          -o offset   clipping offset (required for inline equations)
 rem          [-H /home/dir] (optional) directory to be included in tex search path
 rem
-rem 
-
 rem This batch file REQUIRES that the following folders (directories) 
 rem  were already added to the PATH:
 rem  - folder where the LaTeX executable resides
@@ -60,22 +58,18 @@ IF ERRORLEVEL 1 set inline=0
 set TEXINPUTS=%th%
 latex -quiet --interaction batchmode %fn%
 set TEXINPUTS=
-
 IF NOT EXIST %fn%.dvi GOTO ERR2
 
 dvips -q -o %fn%.eps %fn%.dvi
 
 :NOTEX
 IF NOT EXIST %fn%.eps GOTO ERR3
-
 call eps2eps %fn%.eps tmp1.eps
 convert -crop 0x0 -density %dn%x%dn% tmp1.eps %fn%.png
 del tmp1.eps
-
 IF NOT EXIST %fn%.png GOTO ERR4
 
 IF %inline%==0 GOTO NOIN
-
 pngtopnm %fn%.png > %fn%.pgm
 pnmcut -left %of% %fn%.pgm | pnmcrop -left | pnmtopng > %fn%.png
 del %fn%.pgm
