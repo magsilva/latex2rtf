5456dbe771ee3388c01b080eb83ead25d1bea8ec
whennings <whennings@e562ef82-0334-0410-8083-dbe22b1e9a75>
2007-04-24 12:06:41 +0000

    WH: added l2rprep.bat from DOS distribution
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@560 e562ef82-0334-0410-8083-dbe22b1e9a75

---
+++ scripts/l2rprep.bat
@@ -0,0 +1,21 @@
+rem set folder containing latex2rt.exe and netpbm executables here:
+set l2rpath=C:\l2rtest
+
+set rtfpath=%l2rpath%\cfg
+
+rem set folder containing Ghostscript executables here:
+%l2rpath%\which gswin32c >nul
+if errorlevel 1 SET PATH=C:\Progra~1\gs\gs8.54\bin;C:\Progra~1\gs\gs8.54\lib;%PATH%
+
+rem set folder containing ImageMagick executables here:
+%l2rpath%\which identify >nul
+if errorlevel 1 SET PATH=C:\Progra~1\ImageM~1;%PATH%
+
+rem set folder containing LaTeX and dvips executables here:
+%l2rpath%\which latex >nul
+if errorlevel 1 SET PATH=C:\MiKTeX25\miktex\bin;%PATH%
+
+%l2rpath%\which latex2rt >nul
+if errorlevel 1 SET PATH=%l2rpath%;%PATH%
+
+set l2rpath=
