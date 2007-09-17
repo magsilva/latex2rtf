44e7149372d8d70d71625f6f15cb5d2a6cf0ea11
whennings <whennings@e562ef82-0334-0410-8083-dbe22b1e9a75>
2007-09-17 15:52:52 +0000

    WH: update of the DOS scripts
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@660 e562ef82-0334-0410-8083-dbe22b1e9a75

--- scripts/l2rprep.bat
+++ scripts/l2rprep.bat
@@ -1,11 +1,11 @@
 rem set folder containing latex2rt.exe and netpbm executables here:
-set l2rpath=C:\l2rtest
+set l2rpath=C:\l2r
 
 set rtfpath=%l2rpath%\cfg
 
 rem set folder containing Ghostscript executables here:
 %l2rpath%\which gswin32c >nul
-if errorlevel 1 SET PATH=C:\Progra~1\gs\gs8.54\bin;C:\Progra~1\gs\gs8.54\lib;%PATH%
+if errorlevel 1 SET PATH=C:\Progra~1\gs\gs8.60\bin;C:\Progra~1\gs\gs8.60\lib;%PATH%
 
 rem set folder containing ImageMagick executables here:
 %l2rpath%\which identify >nul
@@ -13,7 +13,7 @@ if errorlevel 1 SET PATH=C:\Progra~1\ImageM~1;%PATH%
 
 rem set folder containing LaTeX and dvips executables here:
 %l2rpath%\which latex >nul
-if errorlevel 1 SET PATH=C:\MiKTeX25\miktex\bin;%PATH%
+if errorlevel 1 SET PATH=C:\MiKTeX26\miktex\bin;%PATH%
 
 %l2rpath%\which latex2rt >nul
 if errorlevel 1 SET PATH=%l2rpath%;%PATH%
