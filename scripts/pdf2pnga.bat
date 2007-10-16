29f8414709ee00d01a1d6413e739cbb112b43f25
whennings <whennings@e562ef82-0334-0410-8083-dbe22b1e9a75>
2007-10-16 07:32:17 +0000

    WH: latex2pnga script tests availability of Ghostscript or sips     and issues system dependent call; unneeded scripts deleted
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@719 e562ef82-0334-0410-8083-dbe22b1e9a75

--- scripts/pdf2pnga.bat
+++
@@ -1,8 +0,0 @@
-@echo off 
-@rem Convert PDF to png.
-
-echo -dNOPAUSE -dBATCH -dSAFER -sDEVICE#pngalpha -r%3 >_.at
-
-rem Watcom C deletes = signs, so use # instead.
-gswin32c -q -sOutputFile#%2 @_.at %1
-if exist _.at erase _.at
