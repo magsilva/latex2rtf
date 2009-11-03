7072029cda24a105d01d8f6c860088bc0b31d424
whennings <whennings@e562ef82-0334-0410-8083-dbe22b1e9a75>
2009-11-03 10:50:05 +0000

    WH: modified bib_apacite2.tex to work around babel bug on MiKTeX2.8
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@919 e562ef82-0334-0410-8083-dbe22b1e9a75

--- scripts/l2rprep.bat
+++ scripts/l2rprep.bat
@@ -5,7 +5,7 @@ set rtfpath=%l2rpath%\cfg
 
 rem set folder containing Ghostscript executables here:
 %l2rpath%\which gswin32c >nul
-if errorlevel 1 SET PATH=C:\Progra~1\gs\gs8.60\bin;C:\Progra~1\gs\gs8.60\lib;%PATH%
+if errorlevel 1 SET PATH=C:\Progra~1\gs\gs8.70\bin;C:\Progra~1\gs\gs8.70\lib;%PATH%
 
 rem set folder containing ImageMagick executables here:
 %l2rpath%\which identify >nul
@@ -13,7 +13,7 @@ if errorlevel 1 SET PATH=C:\Progra~1\ImageM~1;%PATH%
 
 rem set folder containing LaTeX and dvips executables here:
 %l2rpath%\which latex >nul
-if errorlevel 1 SET PATH=C:\MiKTeX26\miktex\bin;%PATH%
+if errorlevel 1 SET PATH=C:\MiKTeX2.8\miktex\bin;%PATH%
 
 %l2rpath%\which latex2rt >nul
 if errorlevel 1 SET PATH=%l2rpath%;%PATH%
