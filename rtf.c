9ae91ba09e72ad0eaf3b8b40fdb3d5f77f83242d
paaguti <paaguti@e562ef82-0334-0410-8083-dbe22b1e9a75>
2010-01-31 06:52:44 +0000

    Not yet included in Makefile for compilation. It's just a proposal of how we could create an RTF layer
    git-svn-id: https://latex2rtf.svn.sourceforge.net/svnroot/latex2rtf/trunk@1059 e562ef82-0334-0410-8083-dbe22b1e9a75

---
+++ rtf.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+#include "main.h"
+#include "utils.h"
+#include "convert.h"
+#include "labels.h"
+#include "rtf.h"
+
+/******************************************************************************
+ purpose: emit RTF code for a bookmark
+ ******************************************************************************/
+void emitBookmark(char *extra, char *presignet,char *signet,char *text)
+{
+    fprintRTF ("{%s\\*\\bkmkstart %s%s}", extra,presignet,signet);
+    ConvertString(text);
+    fprintRTF ("{\\*\\bkmkend %s%s}", presignet,signet);
+}
+/******************************************************************************
+ purpose: emit RTF code to reference a bookmark
+ ******************************************************************************/
+void InsertReference(char *text,
+		     referenceGetter *getRef)
+{
+    char *s = getRef(text);
+    ConvertString(NULL != s ? s : "\\textbf{??}");
+    strfree(s);
+}
+
+/*
+  referenceBookmark("REF","\\\\* MERGEFORMAT","BM",signet,text,getLabelSection);
+  referenceBookmark("REF","\\\\* MERGEFORMAT","BIB_",signet,t,NULL);
+  referenceBookmark("PAGEREF","\\\\p","BM",signet,text,NULL);
+  referenceBookmark("PAGEREF","\\\\* MERGEFORMAT","BM",signet,text,NULL);
+*/
+static void callBookmark(char *pre,
+			 char *post,
+			 char *type,
+			 char *signet,
+			 char *text,
+			 referenceGetter *getRef)
+{
+    fprintRTF("{\\field{\\*\\fldinst{\\lang1024 %s %s%s %s }}",
+	      pre,type,signet,post);
+    
+    if (NULL != text) {
+	fprintRTF ("{\\fldrslt{");
+	if (NULL == getRef) {
+	    fprintRTF(text);
+	} else {
+	    InsertReference(text,getRef);
+	}
+	fprintRTF("}}");
+    }
+    fprintRTF("}");
+}
+
+void referenceBookmark(char *type,
+		       char *signet,
+		       char *text,
+		       referenceGetter *getRef)
+{
+    callBookmark("REF","\\\\* MERGEFORMAT",type,signet,text,getRef);
+}
+
+void pageBookmark(char *type,
+		  char *signet)
+{
+    callBookmark("PAGEREF","\\\\* MERGEFORMAT",type,signet,NULL,NULL);
+}
+ 
---
+++ rtf.h
@@ -0,0 +1,23 @@
+#ifndef _INCLUDED_RTF_H
+#define _INCLUDED_RTF_H 1
+
+#include "labels.h"
+
+void referenceBookmark(/* char *post, */
+		       char *type,
+		       char *signet,
+		       char *text,
+		       referenceGetter *getRef);
+
+void pageBookmark(/* char *post, */
+		  char *type,
+		  char *signet
+		  /* , char *text,
+		     referenceGetter *getRef */);
+
+void InsertReference(char *text,
+		     referenceGetter *getRef);
+
+void emitBookmark(char *extra, char *presignet,char *signet,char *text);
+
+#endif
